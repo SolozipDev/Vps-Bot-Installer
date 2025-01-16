390cd6c0768b2cc633aadbb9c0b5d00895db81bb
SolozipDev <solozip2009@hotmail.com>
2025-01-16 19:55:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+apt install wget -y
+clear
 
 echo "MADE BY SOLOZIP"
 
-echo Make your own Free VPS Hosting, Dont Allow Mining!
+echo Create your own free VPS Hosting!
 
-read -p "Are you sure you want to proceed? Agree to not allow mining (y/n): " -n 1 -r
+read -p "Are you sure you want to continue? Agree to create or not? (y/n): " -n 1 -r
 echo
 
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
@@ -12,34 +14,36 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
 fi
 
-cd ~
+clear
 
-echo "Installing python3-pip and docker."
+echo "Install python3-pip and docker."
 sudo apt update
-sudo apt install -y python3-pip docker.io
-echo Installed successfully
+sudo apt install -y python3 python3-pip docker.io
+echo "Installation successful"
 
-echo "Writing Dockerfile..."
-cat <<EOF > Dockerfile
-FROM ubuntu:22.04
+clear
 
-RUN apt update
-RUN apt install -y tmate
-EOF
+echo "Install the necessary files"
+wget https://raw.githubusercontent.com/SolozipDev/Vps-Bot-Files/refs/heads/main/Dockerfile1
+wget https://raw.githubusercontent.com/SolozipDev/Vps-Bot-Files/refs/heads/main/Dockerfile2
+wget https://raw.githubusercontent.com/SolozipDev/Vps-Bot-Files/refs/heads/main/main.py
+echo "Installation successful"
 
-echo Made successfully - Building Docker image.
-echo "Building Docker Image"
-sudo docker build -t ubuntu-22.04-with-tmate .
-echo Built successfully
-echo "Downloading main.py from the GitHub repository..."
-wget -O main.py https://raw.githubusercontent.com/SolokingzzZ/VPS-DC-BOT-V1-FILES/refs/heads/main/v3ds
-echo Downloaded successfully
-echo "Installing Python packages: discord and docker..."
-pip3 install discord docker
-echo "Please enter your Discord bot token, Make a bot at discord.dev and get the token, You dont need any intents:"
-read -r DISCORD_TOKEN
-echo "Updating main.py with the provided Discord token..."
-sed -i "s/TOKEN = ''/TOKEN = '$DISCORD_TOKEN'/" main.py
-echo "Starting the Discord bot..."
-echo "To start the bot in the future, run: python3 main.py"
+clear
+
+echo "Install the newly loaded files"
+docker build -t ubuntu-22.04-with-tmate -f Dockerfile1 . && docker build -t debian-with-tmate -f Dockerfile2 .
+pip install docker discord
+
+clear
+
+echo "Enter the discord bot token"
+read -r TOKEN
+echo "Updating main.py with token bot Discord is provided..."
+sed -i "s/TOKEN = ''/TOKEN = '$TOKEN'/" main.py
+
+clear
+
+echo "Let's start the bot"
+echo "python3 main.py"
 python3 main.py
